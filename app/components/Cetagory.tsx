9f8869bedea2a88a8d17bba727241f336d03c09c
md_alamin <alamin48081@gmail.com>
2026-02-27 02:33:36 +0600

    done

--- app/components/Cetagory.tsx
+++ app/components/Cetagory.tsx
@@ -1,11 +1,15 @@
+import Link from 'next/link';
 export default function Cetagory() {
   return (
     <div className="w-full h-12 flex items-center justify-center gap-6 px-4 bg-gray-100 text-black text-sm font-medium border-2 border-pink-200">
       {/* Logo / All Button */}
-      <button className="px-3 py-1 bg-pink-500 text-white rounded-md hover:bg-pink-600">
-        All
-      </button>
 
+      <Link href="/">
+        <button className="px-3 py-1 bg-pink-500 text-white rounded-md hover:bg-pink-600">
+          All
+        </button>
+      </Link>
+      
       {/* Category Buttons */}
       <button className="px-3 py-1 rounded-md hover:bg-pink-100">Cafe</button>
       <button className="px-3 py-1 rounded-md hover:bg-pink-100">Home</button>
